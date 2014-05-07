1afb9404a4076abfbea1ca99d3f697fc7afc5ac8
Al Tobey <tobert@gmail.com>
2014-05-07 15:22:11 -0700

    finish building test suites
    The test suite build & data are pretty near their final form.
    
    It's time to move some of the code into the effio lib so this can
    be two tools, one for building the test suite, another for executing.
    That way, the same code can be used for both of those and the graph
    generation.

--- harness/main.go
+++ harness/main.go
@@ -9,7 +9,7 @@ package main
  * The JSON device file is an array of devices and some relevant data. e.g.
  * [
  *   {
- *     "name":       "samsung-840-pro-256",
+ *     "name":       "samsung_840_pro_256",
  *     "device":     "/dev/disk/by-id/ata-Samsung_SSD_840_PRO_Series_S1ATNEAD541857W",
  *     "mountpoint": "/mnt/sda",
  *     "filesystem": "ext4",
@@ -37,6 +37,18 @@ package main
  *   hba:        ioMemory, AHCI, SAS3004, USB3, mixed (for MDRAID)
  *   media:      MLC, Iron (for HDDs), TLC, SLC, Hybrid (SSHD)
  *   blocksize:  `blockdev --getpbsz /dev/sda`
+ *
+ * The fio config file name is stripped of its path and .fio extension to be
+ * used in composite test names. A test against the above device with
+ * "rand_512b_write_iops.fio" will be named
+ * "rand_512b_write_iops-samsung_840_pro_256".
+ *
+ * 2014-05-07T19:21:00Z/
+ *   rand_512b_write_iops-samsung_840_pro_256/
+ *     config.fio - the fio configuration file
+ *     harness.json  - a dump of all data used to generate the test
+ *     output.json  - json output from fio --output-format=json
+ *     run.sh     - the exact command used to run fio
  */
 
 import (
@@ -48,6 +60,8 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
+	"text/template"
 	"time"
 )
 
@@ -72,9 +86,9 @@ type DeviceList []Device
 // fio has some built-in variable expansion but I want to save the artifacts
 // to a git repo
 type FioTmpl struct {
-	Filename string // full path to the file
-	Basename string // used to generate output filenames
-	Template string // raw template data
+	Filename string             // full path to the source file
+	Name     string             // used to generate output filenames
+	Template *template.Template `json:"-"`
 }
 
 type FioTmplList []FioTmpl
@@ -84,9 +98,13 @@ type Test struct {
 	Name     string    // name to be used in tests, files, etc.
 	Time     time.Time // time the test was generated / run
 	Dir      string    // directory for writing configs, logs, etc.
+	FioCmd   string    // the fio command to execute to run the test
 	BWLog    string    // filename for the bandwidth log
 	LatLog   string    // filename for the latency log
 	IopsLog  string    // filename for the iops log
+	FioFile  string    // generated fio config file name
+	DataFile string    // dump the test data (this struct) to this file
+	CmdFile  string    // write the exact fio command used to this file
 	Template FioTmpl   // template info struct
 	Dev      Device    // device info struct
 }
@@ -102,7 +120,7 @@ func init() {
 	if err != nil {
 		devfile = "devices"
 	}
-	devfile = fmt.Sprintf("%s.json", devfile)
+	devfile = fmt.Sprintf("./%s.json", devfile)
 
 	flag.StringVar(&devJsonFlag, "devs", devfile, "A JSON file containing device metadata")
 	flag.StringVar(&confDirFlag, "conf", "./fio_configs", "A directroy containing fio config templates")
@@ -112,48 +130,114 @@ func init() {
 func main() {
 	flag.Parse()
 
+	// change any relative paths to absolute paths
+	devJson := required(filepath.Abs(devJsonFlag))
+	confDir := required(filepath.Abs(confDirFlag))
+	outPath := required(filepath.Abs(outPathFlag))
+
+	// load device data from json
+	devs := loadDevJson(devJson)
+
+	// load the fio config templates into memory
+	templates := loadFioTmpl(confDir)
+
+	// build up a test suite of devs x templates
+	suite := buildTestSuite(devs, templates, outPath)
+
+	// write out all the files
+	for _, test := range suite {
+		err := os.MkdirAll(test.Dir, 0755)
+		if err != nil {
+			log.Fatalf("Failed to create test directory '%s': %s\n", test.Dir, err)
+		}
+
+		test.writeFioFile()
+		test.writeDataFile()
+		test.writeCmdFile()
+	}
+
+	// TODO: run the tests!
+}
+
+// for each device/fio config combination, create a config file in
+// a new directory named <iso8601 date>/<name> with one directory
+// per test so fio can be excuted in those directories, keeping
+// data generated along side the configs
+func buildTestSuite(devs DeviceList, templates FioTmplList, outPath string) (suite TestSuite) {
 	// get the current time once and use it for the whole suite
 	now := time.Now()
 
-	// load device data from json
-	devs := loadDevJson(devJsonFlag)
-
-	// load the fio config templates into memory
-	templates := loadFioTmpl(confDirFlag)
-
-	// for each device/fio config combination, create a config file in
-	// a new directory named <name>-<iso8601 date> with one directory
-	// per test so fio can be excuted in those directories, keeping
-	// data generated along side the configs
-	suite := TestSuite{}
 	for _, tp := range templates {
 		for _, dev := range devs {
-			testName := fmt.Sprintf("%s-%s", dev.Name, tp.Basename)
-			testDir := path.Join(outPathFlag, testName)
+			testName := fmt.Sprintf("%s-%s", dev.Name, tp.Name)
+			testDir := path.Join(outPath, testName)
+			fioJson := path.Join(outPath, "output.json")
+			fioConf := path.Join(testDir, "config.fio")
+			cmd := fmt.Sprintf("fio --output-format=json --output=%s %s", fioJson, fioConf)
 
 			// fio adds _$type.log to log file names so only provide the base name
 			test := Test{
 				Name:     testName,
 				Time:     now,
 				Dir:      testDir,
-				BWLog:    fmt.Sprintf("bw-%s", testName),
-				LatLog:   fmt.Sprintf("lat-%s", testName),
-				IopsLog:  fmt.Sprintf("iops-%s", testName),
+				FioCmd:   cmd,
+				FioFile:  fioConf,
+				BWLog:    path.Join(testDir, "bw"),
+				LatLog:   path.Join(testDir, "lat"),
+				IopsLog:  path.Join(testDir, "iops"),
+				DataFile: path.Join(testDir, "harness.json"),
+				CmdFile:  path.Join(testDir, "run.sh"),
 				Template: tp,
 				Dev:      dev,
 			}
+
 			suite = append(suite, test)
 		}
 	}
 
-	// TODO: generate fio config, shell command, dump test to JSON
-	fmt.Printf("%v\n", suite)
+	return suite
+}
 
-	// TODO: run the tests!
+func (test *Test) writeFioFile() {
+	fd, err := os.OpenFile(test.FioFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatalf("Failed to create fio config file '%s': %s\n", test.FioFile, err)
+	}
+	defer fd.Close()
+
+	err = test.Template.Template.Execute(fd, test)
+	if err != nil {
+		log.Fatalf("Template execution failed: %s\n", err)
+	}
+}
+
+func (test *Test) writeDataFile() {
+	js, err := json.MarshalIndent(test, "", "  ")
+	if err != nil {
+		log.Fatalf("Failed to encode test data as JSON: %s\n", err)
+	}
+
+	// MarshalIndent does not follow the final brace with a newline
+	js = append(js, byte('\n'))
+
+	err = ioutil.WriteFile(test.DataFile, js, 0644)
+	if err != nil {
+		log.Fatalf("Failed to write JSON data file '%s': %s\n", test.DataFile, err)
+	}
+}
+
+func (test *Test) writeCmdFile() {
+	fd, err := os.OpenFile(test.CmdFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0755)
+	if err != nil {
+		log.Fatalf("Failed to create command file '%s': %s\n", test.CmdFile, err)
+	}
+	defer fd.Close()
+
+	fmt.Fprintf(fd, "#!/bin/bash -x\n%s\n", test.FioCmd)
 }
 
 func loadDevJson(fname string) (devs DeviceList) {
-	mdbuf, err := ioutil.ReadFile(devJsonFlag)
+	mdbuf, err := ioutil.ReadFile(fname)
 	if err != nil {
 		log.Fatalf("Could not read '%s': %s\n", fname, err)
 	}
@@ -179,7 +263,11 @@ func loadFioTmpl(dir string) (out FioTmplList) {
 				log.Fatalf("Could not read fio config '%s': %s", fpath, err)
 			}
 
-			out = append(out, FioTmpl{fpath, fname, string(data)})
+			// remove the .fio to get the base filename to use as a generic name string
+			name := strings.TrimSuffix(fname, ext)
+			tmpl := template.Must(template.New(name).Parse(string(data)))
+
+			out = append(out, FioTmpl{fpath, name, tmpl})
 		}
 
 		return nil
@@ -191,4 +279,12 @@ func loadFioTmpl(dir string) (out FioTmplList) {
 	}
 
 	return out
+}
+
+func required(data string, err error) string {
+	if err != nil {
+		log.Fatalf("BUG: Required operation failed with error: %s\n", err)
+	}
+
+	return data
 }

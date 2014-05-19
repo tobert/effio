396b3ad6b0e352d88227ecf6486a465d5d4afefa
Al Tobey <tobert@gmail.com>
2014-05-19 16:41:01 +0000

    Clean up argument parsing.
    main.go is now only an entry point to the effio package.
    
    Made commands with < 3 args work without bogus filler args.
    
    effio -h / effio help / effio -help are now less malevolent

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"./src/effio"
-	"log"
 	"os"
 )
 
@@ -19,17 +18,5 @@ import (
 
 func main() {
 	cmd := effio.NewCmd(os.Args)
-
-	switch cmd.Command {
-	case "make":
-		cmd.MakeSuite()
-	case "run":
-		cmd.RunSuite()
-	case "inventory":
-		cmd.Inventory()
-	case "mountall":
-		cmd.Mountall()
-	default:
-		log.Fatalf("Invalid subcommand '%s'.\n%s\n", cmd.Command, cmd.Usage())
-	}
+	cmd.Run()
 }
--- src/effio/cmd.go
+++ src/effio/cmd.go
@@ -18,14 +18,52 @@ type Cmd struct {
 // The first argument is always considered a subcommand but is not
 // parsed by this package. It is meant to be used with the flag package.
 // Usage: cmd := effio.NewCmd(os.Args)
-func NewCmd(args []string) Cmd {
-	if len(args) < 3 {
-		log.Fatalf("Not enough arguments! (must be at least 2)")
+func NewCmd(args []string) (cmd Cmd) {
+	if len(args) < 2 {
+		log.Fatalf("subcommand required: make|run|inventory|mountall\n%s\n", cmd.Usage())
 	}
 
-	fs := flag.NewFlagSet(args[0], flag.ExitOnError)
+	cmd.Process = args[0]
+	cmd.Command = args[1]
 
-	return Cmd{args[0], args[1], args[2:], fs}
+	if len(args) > 2 {
+		cmd.Args = args[2:]
+	} else {
+		cmd.Args = []string{}
+	}
+
+	cmd.FlagSet = flag.NewFlagSet(cmd.Process, flag.ExitOnError)
+
+	return cmd
+}
+
+func (cmd *Cmd) MinArgs(required int) {
+	if len(cmd.Args) < required {
+		log.Fatalf("Not enough arguments '%s %s' (%d required)", cmd.Process, cmd.Command, required)
+	}
+}
+
+func (cmd *Cmd) Run() {
+	switch cmd.Command {
+	case "make":
+		cmd.MinArgs(3)
+		cmd.MakeSuite()
+	case "run":
+		cmd.MinArgs(1)
+		cmd.RunSuite()
+		cmd.RunSuite()
+	case "inventory":
+		cmd.MinArgs(0)
+		cmd.Inventory()
+	case "mountall":
+		cmd.MinArgs(0)
+		cmd.Mountall()
+	case "help", "-h", "-help", "--help":
+		fmt.Fprintln(os.Stderr, cmd.Usage())
+		os.Exit(2)
+	default:
+		log.Fatalf("Invalid subcommand '%s'.\n%s\n", cmd.Command, cmd.Usage())
+	}
 }
 
 // TODO: fill in usage when things settle down

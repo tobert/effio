d8b27ac83730a0026eba9e646d3b13363eb617dd
Al Tobey <tobert@gmail.com>
2014-06-04 22:28:32 +0000

    remove dead code

--- src/effio/plotinum.go
+++ src/effio/plotinum.go
@@ -35,43 +35,6 @@ func (test *Test) LatLogPath(suite_path string) string {
 	return path.Join(tpath, fmt.Sprintf("%s_lat.log", test.FioLatLog))
 }
 
-func (test *Test) GraphAll(suite_path string) {
-	latlog := LoadCSV(test.LatLogPath(suite_path))
-	// latlog is huge on fast devices, trim it down so plotinum doesn't freak out
-	//func (lrs LatRecs) Histogram(sz int) (out LatRecs) {
-	hgram := latlog.Histogram(200)
-	test.LineGraph(hgram, "Time", "Latency (usec)")
-}
-
-func (test *Test) LineGraph(data plotter.XYer, xlabel string, ylabel string) {
-	p, err := plot.New()
-	if err != nil {
-		log.Fatalf("Error creating new plot: %s\n", err)
-	}
-
-	p.Title.Text = fmt.Sprintf("Latency %s", test.Device)
-	p.X.Label.Text = xlabel
-	p.Y.Label.Text = ylabel
-
-	p.Add(plotter.NewGrid())
-
-	log.Printf("Adding data with l, err := plotter.NewLine(data)\n")
-	l, err := plotter.NewLine(data)
-	if err != nil {
-		log.Fatalf("Error adding line to plot: %s\n", err)
-	}
-	l.LineStyle.Width = vg.Points(1)
-
-	p.Add(l)
-
-	log.Printf("saving graph to linegraph.png\n")
-	err = p.Save(10, 10, "linegraph.png")
-	if err != nil {
-		log.Fatalf("Failed to save linegraph.png: %s\n", err)
-	}
-	log.Printf("all done\n")
-}
-
 // draws a bar graph displaying the sizes of the lat_lat.log files across
 // all tests
 // TODO: figure out how to make the bar width respond to the graph width
